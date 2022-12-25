02436b8c58239c99cdc24c96c370d64673e7ee53
Felipe <felipedantas184@gmail.com>
2022-12-25 17:20:50 -0300

    good job man

--- components/AdminPage/AdminStyles.js
+++ components/AdminPage/AdminStyles.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
-export const BookingsSection = styled.section`
+export const AdminSection = styled.section`
 	padding: 40px 0;
   min-height: 70vh;
 `
-export const BookingsContainer = styled.div`
+export const AdminContainer = styled.div`
 	max-width: 1024px;
   margin: auto;
 
@@ -12,7 +12,7 @@ export const BookingsContainer = styled.div`
     padding: 0 16px;
   }
 `
-export const BookingsHeading = styled.div`
+export const AdminHeading = styled.div`
 	text-align: center;
   width: 60%;
   margin: auto;
@@ -21,19 +21,19 @@ export const BookingsHeading = styled.div`
     width: 100%;
   }
 `
-export const BookingsTitle = styled.h1`
+export const AdminTitle = styled.h1`
   color: #2d3954;
   font-weight: 600;
   text-transform: capitalize;
   font-size: 32px;
 `
-export const BookingsSubtitle = styled.p`
+export const AdminSubtitle = styled.p`
 	color: #72809d;
 `
-export const BookingsCards = styled.div`
-	margin-top: 50px;
+export const AdminCards = styled.div`
+	margin-top: 24px;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   grid-gap: 30px;
 
   @media screen and (max-width: 800px) {
@@ -41,45 +41,45 @@ export const BookingsCards = styled.div`
     margin-top: 32px;
   }
 `
-export const BookingCard = styled.div`
+export const AdminBookingCard = styled.div`
   box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
   border-radius: 8px;
   background-color: #F6F6F6;
 `
-export const BookingCardImage = styled.div`
+export const AdminBookingCardImage = styled.div`
 	position: relative;
   width: 100%;
-  height: 250px;
+  height: 200px;
   border-radius: 8px 8px 0 0;
   overflow: hidden;
 `
-export const BookingCardText = styled.div`
+export const AdminBookingCardText = styled.div`
   display: flex;
   flex-direction: column;
 `
 //INÍCIO
 
 
-export const BookingCardTextCategory = styled.div`
+export const AdminBookingCardTextCategory = styled.div`
 	align-items: center;
   display: flex;
   justify-content: space-between;
 `
-export const BookingCardTitle = styled.h4`
+export const AdminBookingCardTitle = styled.h4`
 	font-weight: 500;
   margin-top: 10px;
 `
-export const BookingCardLocation = styled.p`
+export const AdminBookingCardLocation = styled.p`
 	color: #72809d;
 `
 
 
 //FIM
 
-export const BookingInfo = styled.div`
+export const AdminBookingInfo = styled.div`
   margin-bottom: 8px;
 `
-export const BookingTitle = styled.h2`
+export const AdminBookingTitle = styled.h2`
   padding: 16px 20px 0 20px;
   margin-bottom: 12px !important;
 
@@ -88,29 +88,16 @@ export const BookingTitle = styled.h2`
   font-weight: 600 !important;
   font-size: 16px;
 `
-export const BookingBox = styled.div`
+export const AdminBookingBox = styled.div`
   display: flex;
   flex-direction: column;
 
   padding: 0 20px 0 20px;
   margin-bottom: 16px;
 
-  @media screen and (max-width: 800px) {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-`
-export const BookingLabel = styled.p`
-  margin-top: 0 !important;
-  margin-bottom: 0 !important;
-  font-weight: 600;
-  font-size: 14px;
-`
-export const BookingName = styled.p`
-  margin-top: 0 !important;
-  margin-bottom: 0 !important;
-  font-size: 14px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 
   @media screen and (max-width: 800px) {
     white-space: nowrap;
@@ -118,12 +105,33 @@ export const BookingName = styled.p`
     text-overflow: ellipsis;
   }
 `
-export const BookingBoxHorizontal = styled.div`
+export const AdminBookingLabel = styled.p`
+  margin-top: 0 !important;
+  margin-bottom: 0 !important;
+  font-weight: 600;
+  font-size: 14px;
+`
+export const AdminBookingName = styled.p`
+  margin-top: 0 !important;
+  margin-bottom: 0 !important;
+  font-size: 14px;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  @media screen and (max-width: 800px) {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`
+export const AdminBookingBoxHorizontal = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
 `
-export const BookingBoxHorizontalGrid2 = styled.div`
+export const AdminBookingBoxHorizontalGrid2 = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
 `
@@ -179,5 +187,11 @@ export const ImageWrapper = styled.div`
   @media screen and (max-width: 720px) {
     width: 215px;
     height: 160px;
+  }
+`
+
+export const AdminTotalAmountTitle = styled.h2`
+  @media screen and (max-width: 720px) {
+    font-size: 16px;
   }
 `
--- pages/admin.js
+++ pages/admin.js
@@ -2,6 +2,14 @@ import MyBookingsPage from "../components/MyBookings";
 import { collection, getDocs } from 'firebase/firestore';
 import Layout from '../components/Layout/Layout';
 import fireDB from '../firebase/initFirebase';
+import { BookingBox, BookingBoxHorizontalGrid2, BookingCard, BookingCardImage, BookingCardText, BookingInfo, BookingLabel, BookingName, BookingsCards, BookingsContainer, BookingsHeading, BookingsSection, BookingsSubtitle, BookingsTitle, BookingTitle, RoomCardButton, RoomCardButtonContainer } from "../components/MyBookings/MyBookingsStyles";
+import Image from "next/image";
+import { useState } from "react";
+import moment from "moment";
+import { DatePicker } from "antd";
+import 'antd/dist/antd.css'
+import locale from 'antd/lib/date-picker/locale/pt_BR';
+import { AdminBookingBox, AdminBookingBoxHorizontalGrid2, AdminBookingCard, AdminBookingCardImage, AdminBookingCardText, AdminBookingInfo, AdminBookingLabel, AdminBookingName, AdminBookingTitle, AdminCards, AdminContainer, AdminHeading, AdminSection, AdminSubtitle, AdminTitle, AdminTotalAmountTitle } from "../components/AdminPage/AdminStyles";
 
 export async function getServerSideProps() {
   const firebaseBookings = await getDocs(collection(fireDB, "bookings"));
@@ -47,11 +55,132 @@ export async function getServerSideProps() {
 }
 
 const Admin = ({ bookings, rooms, users }) => {
-  return ( 
+  const [selectedMonth, setSelectedMonth] = useState(moment().utcOffset('-03:00').format('DD-MM-YYYY hh:mm:ss a').slice(3,10))
+  const disabledDate = (current) => {
+    return current && current < moment().endOf("day")
+  };
+
+  const getTotalAmount = (selectedMonth) => {
+    const filteredMonth = bookings.filter((item) => item.from.slice(3, 10) == selectedMonth)
+    const value = filteredMonth.reduce((prev, curr) => prev + curr.amount, 0)
+
+    return value
+  }
+
+  const getRoomName = (roomId) => {
+    const room = rooms.filter((room) => room.id == roomId)
+    const roomName = (room[0].title)
+
+    return roomName
+  }
+  const getRoomImage = (roomId) => {
+    const room = rooms.filter((room) => room.id == roomId)
+    const roomImage = (room[0].imageurl)
+
+    return roomImage
+  }
+  const getUserName = (userId) => {
+    const user = users.filter((user) => user.id == userId)
+    console.log(user)
+    const userName = (user[0].name + ' ' + user[0].surname)
+
+    return userName
+  }
+  const getUserEmail = (userId) => {
+    const user = users.filter((user) => user.id == userId)
+    console.log(user)
+    const userEmail = (user[0].email)
+
+    return userEmail
+  }
+
+  async function deleteData(bookingId, roomId, bookingFrom, bookingTo) {
+    try {
+      if (confirm("Você tem certeza de que deseja cancelar esta reserva?" + roomId) == true) {
+        await deleteDoc(doc(fireDB, "bookings", bookingId)).then(
+          updateDoc(doc(fireDB, "rooms", roomId), {
+            currentBookings: arrayRemove({
+              bookingId: bookingId,
+              fromdate: bookingFrom,
+              todate: bookingTo
+            })
+          })
+        )
+        alert("Reserva cancelada!")
+        location.reload()
+      }
+    } catch (error) {
+      console.log(error)
+      alert(error)
+    }
+  }
+
+  const onChange = (date, dateString) => {
+    setSelectedMonth(dateString)
+  };
+
+  return (
     <Layout>
-      <MyBookingsPage bookings={bookings} rooms={rooms} users={users} />
+      <AdminSection>
+        <AdminContainer>
+          <AdminHeading>
+            <AdminTitle>Checar Reservas</AdminTitle>
+            <AdminSubtitle>Confira aqui as reservas no seu hotel.</AdminSubtitle>
+            <DatePicker picker="month" inputReadOnly={true} format="MM-YYYY" locale={locale} allowClear={false} disabledDate={disabledDate} onChange={onChange}/>
+          </AdminHeading>
+          <div style={{width: '100%', color: '#13131A', marginTop: 24, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+            <AdminTotalAmountTitle>Total a Receber em <br/> {selectedMonth} </AdminTotalAmountTitle>
+            <h3>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', }).format(getTotalAmount(selectedMonth))}</h3>
+          </div>
+          <AdminCards>
+            {bookings.filter((item) => item.from.slice(3, 10) == selectedMonth)
+              .map((booking) => (
+                <AdminBookingCard key={booking.id}>
+                  <AdminBookingCardImage>
+                    <Image src={'https:' + getRoomImage(booking.roomId)} alt={getRoomName(booking.roomId)} layout='fill' />
+                  </AdminBookingCardImage>
+                  <AdminBookingCardText>
+                    <AdminBookingInfo>
+                      <AdminBookingTitle>{getRoomName(booking.roomId)}</AdminBookingTitle>
+                      <AdminBookingBox>
+                        <AdminBookingLabel>Nome</AdminBookingLabel>
+                        <AdminBookingName>{getUserName(booking.userId)}</AdminBookingName>
+                      </AdminBookingBox>
+                      <AdminBookingBoxHorizontalGrid2>
+                        <AdminBookingBox>
+                          <AdminBookingLabel>Email</AdminBookingLabel>
+                          <AdminBookingName>{getUserEmail(booking.userId)}</AdminBookingName>
+                        </AdminBookingBox>
+                        <AdminBookingBox>
+                          <AdminBookingLabel>Capacidade</AdminBookingLabel>
+                          <AdminBookingName>4 pessoas</AdminBookingName>
+                        </AdminBookingBox>
+                      </AdminBookingBoxHorizontalGrid2>
+                      <AdminBookingBoxHorizontalGrid2>
+                        <AdminBookingBox>
+                          <AdminBookingLabel>Check-In</AdminBookingLabel>
+                          <AdminBookingName>{booking.from}</AdminBookingName>
+                        </AdminBookingBox>
+                        <AdminBookingBox>
+                          <AdminBookingLabel>Check-Out</AdminBookingLabel>
+                          <AdminBookingName>{booking.to}</AdminBookingName>
+                        </AdminBookingBox>
+                      </AdminBookingBoxHorizontalGrid2>
+                    </AdminBookingInfo>
+                  </AdminBookingCardText>
+
+                  <RoomCardButtonContainer>
+                    <span style={{ fontWeight: '500', fontSize: 14 }} >{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', }).format(booking.amount)}</span>
+
+                    <RoomCardButton onClick={() => deleteData(booking.id, booking.roomId, booking.from, booking.to)} >Cancelar Reserva</RoomCardButton>
+                  </RoomCardButtonContainer>
+                </AdminBookingCard>
+              ))}
+          </AdminCards>
+        </AdminContainer>
+      </AdminSection>
     </Layout>
-   );
+  );
 }
- 
+
 export default Admin;
